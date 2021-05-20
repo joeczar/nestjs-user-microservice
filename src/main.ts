b2b23d60b34fad25b6232b71a76d729c6f3e9c8e
Joe Czarnecki <joe.czarnecki@db-n.com>
2021-05-20 21:06:32 +0200

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: 'localhost',
+      port: 4010,
+    },
+  });
+
+  app.startAllMicroservicesAsync();
+  await app.listen(3010);
+}
+bootstrap();
---
+++ src/user/user.entity.ts
@@ -0,0 +1,41 @@
+import { hash } from 'bcrypt';
+import { IsEmail, Min } from 'class-validator';
+import {
+  BeforeInsert,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
+import { UserInterface } from './user.interface';
+
+@Entity()
+@Unique(['username'])
+@Unique(['email'])
+export class User implements UserInterface {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  @Min(8)
+  password: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  @IsEmail()
+  email: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await hash(this.password, 10);
+  }
+}
